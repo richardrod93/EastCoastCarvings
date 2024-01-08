f4e35775abe7284992cdb3854122d7a26ceba3b5
richardrod93 <112108080+richardrod93@users.noreply.github.com>
2024-01-07 22:40:43 -0500

    new file:   .gitattributes 	new file:   .gitignore 	new file:   index.js 	new file:   public/index.html 	new file:   public/style.css

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the index.html file when the root route is accessed
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
